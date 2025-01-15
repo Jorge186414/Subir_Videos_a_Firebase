b940bae0eb1447fa900d58afdd2a523c64f95d2d
Jorge Miranda <jorgaismiranda@gmail.com>
2025-01-14 23:47:26 -0600

    Add: Activity para el inicio de sesion y logica para mostrar la actividad si no hay una sesion

--- app/src/main/java/com/tesji/videos_a_firebase/MainActivity.kt
+++ app/src/main/java/com/tesji/videos_a_firebase/MainActivity.kt
@@ -1,20 +1,32 @@
 package com.tesji.videos_a_firebase
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.tesji.videos_a_firebase.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        comprobarSesion()
+    }
+
+    private fun comprobarSesion(){
+        if(firebaseAuth.currentUser == null){
+            startActivity(Intent(applicationContext, LoginGoogleActivity::class.java))
+            finishAffinity()
         }
     }
 }
