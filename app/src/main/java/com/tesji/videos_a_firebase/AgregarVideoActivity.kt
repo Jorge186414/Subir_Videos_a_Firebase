9e85b2656957abb9c97366dce3a500e4785230eb
Jorge Miranda <jorgaismiranda@gmail.com>
2025-01-15 04:06:05 -0600

    Add: Carga de los videos a Firebase

--- app/src/main/java/com/tesji/videos_a_firebase/AgregarVideoActivity.kt
+++ app/src/main/java/com/tesji/videos_a_firebase/AgregarVideoActivity.kt
@@ -1,6 +1,7 @@
 package com.tesji.videos_a_firebase
 
 import android.app.Activity
+import android.app.ProgressDialog
 import android.content.Intent
 import android.net.Uri
 import android.os.Build
@@ -15,6 +16,9 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.PopupMenu
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
 import com.tesji.videos_a_firebase.databinding.ActivityAgregarVideoBinding
 import com.tesji.videos_a_firebase.databinding.ActivityMainBinding
 
@@ -22,15 +26,77 @@ class AgregarVideoActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityAgregarVideoBinding
     private var uriVideo : Uri ?= null
+    private var titulo : String ?= null
+    private lateinit var progressDialog: ProgressDialog
+    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAgregarVideoBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Por Favor Espere")
+        progressDialog.setMessage("Se esta subiendo el video")
+        progressDialog.setCanceledOnTouchOutside(false)
+
         binding.btnSelectVideo.setOnClickListener{
             videoPickDialog()
         }
+
+        binding.btnUploadVideo.setOnClickListener {
+            titulo = binding.etTituloVideo.text.toString().trim()
+            if(titulo.isNullOrEmpty()){
+                Toast.makeText(applicationContext, "Ingresa un titulo de video", Toast.LENGTH_SHORT).show()
+            }else if(uriVideo == null){
+                Toast.makeText(applicationContext, "Carga un video", Toast.LENGTH_SHORT).show()
+            }else{
+                uploadVideo()
+            }
+        }
+    }
+
+    private fun uploadVideo() {
+        progressDialog.show()
+
+        val autor = firebaseAuth.currentUser!!.displayName
+
+        val tiempo = ""+System.currentTimeMillis()
+        val rutaNombre = "Video/video_${tiempo}"
+        val storageRef = FirebaseStorage.getInstance().getReference(rutaNombre)
+        storageRef.putFile(uriVideo!!)
+            .addOnSuccessListener { task ->
+                val uriTask = task.storage.downloadUrl
+                while (!uriTask.isSuccessful);
+                val downloadUri = uriTask.result.toString()
+                if(uriTask.isSuccessful){
+                    val hashMap = HashMap<String, Any>()
+                    hashMap["id"] = "${tiempo}"
+                    hashMap["titulo"] = "${titulo}"
+                    hashMap["tiempo"] = "${tiempo}"
+                    hashMap["videoUri"] = "${downloadUri}"
+                    hashMap["autor"] = "${autor}"
+
+                    val reference = FirebaseDatabase.getInstance().getReference("Videos")
+                    reference.child(tiempo)
+                        .setValue(hashMap)
+                        .addOnSuccessListener {
+                            progressDialog.dismiss()
+                            Toast.makeText(applicationContext, "Video subido Exitosamente!!", Toast.LENGTH_SHORT).show()
+                            startActivity(Intent(this@AgregarVideoActivity, MainActivity::class.java))
+                            finish()
+                        }
+                        .addOnFailureListener { e ->
+                            progressDialog.dismiss()
+                            Toast.makeText(applicationContext, "${e.message}", Toast.LENGTH_SHORT).show()
+                        }
+            }
+        }
+            .addOnFailureListener{ e ->
+                Toast.makeText(applicationContext, "${e.message}", Toast.LENGTH_SHORT).show()
+            }
     }
 
     private fun videoPickDialog() {
